12b9fcc437ffa25fe5dc54267ce587579b1a4424
cofitost <zxcv091922@gmail.com>
2019-06-14 03:22:56 +0800

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloModule } from 'cofi-hello-app/dist'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HelloModule
   ],
   providers: [],
   bootstrap: [AppComponent]
